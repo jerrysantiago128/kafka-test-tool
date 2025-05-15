05776b3b41df965e9f36ad4c123caaba52f8dce6
Jerry Santiago <jerrysantiago128@gmail.com>
2025-05-15 07:36:24 -0400

    renaming namespace back to com

--- test-suite/Produce.sh
+++ test-suite/Produce.sh
@@ -3,4 +3,4 @@
 dir=$(pwd)
 jar=$(find "$dir" -type f -name "*.jar")
 
-java -cp $dir/../target/kafka-test-tool-0.10.0.jar msps.test.kafka.Producer "$@"
+java -cp $dir/../target/kafka-test-tool-0.10.0.jar com.test.kafka.Producer "$@"
